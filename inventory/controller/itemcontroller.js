8cec0f644f00eae590e8c08929b58778233b35a4
hassan <hchoudhury20@hotmail.com>
2023-08-16 14:46:08 +0100

    set up more controllers

--- inventory/controller/itemcontroller.js
+++ inventory/controller/itemcontroller.js
@@ -1,7 +1,21 @@
-const item = require("../../models/item")
+const item = require("../models/item")
 const asyncHandler = require("express-async-handler");
 
 
 exports.createitem = async (req,res) => {
-res.send("not implemented")
+res.send("not implemented:create item")
 }
+
+exports.viewitem = async (req,res) => {
+    res.send("not implemented:create item")
+}
+
+exports.updateitem = async (req,res) => {
+        res.send("not implemented:create item")
+
+}
+        
+exports.deleteitem = async (req,res) => {
+    res.send("not implemented:create item")
+    }
+    
---
+++ inventory/models/item.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+  description: { type: String, maxLength: 300 },
+  category: { type: String, maxLength: 50 },
+  price: { type: Number, min: 0 },
+  stock: { type: Number, min: 0 },
+  URL: { type: String },
+});
+
+module.exports = mongoose.model("Item", itemSchema);
