3f14b7a2a26b76a328ab2b5795df97c77c4a7c8a
hassan <hchoudhury20@hotmail.com>
2023-08-17 23:59:08 +0100

    add crud operations

--- inventory/controller/itemcontroller.js
+++ inventory/controller/itemcontroller.js
@@ -1,25 +1,56 @@
-const item = require("/Users/hassan/TOP/node/store_inventory/inventory/models/item.js")
+const item = require("../models/item");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
+exports.createitem_get = async (req, res) => {
+  res.render("create", { title: "Add item" });
+};
 
-exports.createitem = async (req,res) => {
-res.send("not implemented:create item")
-}
+exports.createitem_post = [
+  body("name", "Item name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // Handle validation errors
+      // Render the form with sanitized values/error messages
+      res.render("create", {
+        title: "Add item",
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    // Data from form is valid
+    const newItem = new item({ name: req.body.name });
+    await newItem.save();
+
+    // Redirect or send a response
+    res.redirect("/create");
+
+    // newItem.save();
+
+    // Redirect or send a response
+    // res.redirect("/items");
+    // or
+    // res.send("Item created successfully");
+  }),
+];
 
 exports.viewitem = asyncHandler(async (req, res, next) => {
-    const allItems = await item.find({}, "name price")
-    .sort({ name: 1 })
-    .exec();
-  
-    res.render("item", { title: "Items", viewitem: allItems });
-  });
+  const allItems = await item.find({}, "name price").sort({ name: 1 }).exec();
 
-exports.updateitem = async (req,res) => {
-    res.send("not implemented:update item")
+  res.render("item", { title: "Items", viewitem: allItems });
+});
 
-}
-        
-exports.deleteitem = async (req,res) => {
-    res.send("not implemented:delete item")
-    }
-    
+exports.updateitem = async (req, res) => {
+  res.send("Not implemented: Update item");
+};
+
+exports.deleteitem = async (req, res) => {
+  res.send("Not implemented: Delete item");
+};
--- inventory/routes/index.js
+++ inventory/routes/index.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-const item_controller = require("../controller/itemcontroller")
+const item_controller = require("../controller/itemcontroller");
+const item = require('../models/item');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get("/create",item_controller.createitem)
+router.get("/create",item_controller.createitem_get)
+router.post("/create",item_controller.createitem_post)
+
 router.get("/view",item_controller.viewitem)
 router.get("/update",item_controller.updateitem)
 router.get("/delete",item_controller.deleteitem)
